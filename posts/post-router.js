9753598d029a8d6b186aef12ed611a76f75310f8
nuvallo <nuvallo.inc@gmail.com>
2020-04-12 17:59:11 -0400

    GET post by ID complete

--- posts/post-router.js
+++ posts/post-router.js
@@ -15,4 +15,21 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:id", (req, res) => {
+  posts
+    .findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        // this isn't displaying (come back)
+        res.status(404).json({ message: "Post not found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Error retrieving the posts" });
+    });
+});
+
 module.exports = router;
