949060d31e6179129e468bc7033d43eaaebd1fea
nuvallo <nuvallo.inc@gmail.com>
2020-04-12 18:33:41 -0400

    DELETE and PUT complete for posts

--- posts/post-router.js
+++ posts/post-router.js
@@ -2,7 +2,9 @@ const express = require("express");
 const posts = require("../data/db.js");
 const router = express.Router();
 
-//GET request to /api/posts
+// Request to /api/posts
+
+// Get all posts
 router.get("/", (req, res) => {
   posts.find().then((posts) => {
     res
@@ -15,6 +17,7 @@ router.get("/", (req, res) => {
   });
 });
 
+// Get post
 router.get("/:id", (req, res) => {
   posts
     .findById(req.params.id)
@@ -32,6 +35,7 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// Adding a new post
 router.post("/", (req, res) => {
   const title = req.body.title;
   const contents = req.body.contents;
@@ -40,16 +44,54 @@ router.post("/", (req, res) => {
     return res.status(400).json({
       mesage: "Post needs a title or content",
     });
+  } else {
+    posts
+      .insert(req.body)
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: "Error adding post" });
+      });
   }
-  posts
-    .insert(req.body)
-    .then((post) => {
-      res.status(201).json(post);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({ message: "Error adding post" });
+});
+
+// Updating Post
+router.put("/:id", (req, res) => {
+  const title = req.body.title;
+  const contents = req.body.contents;
+
+  if (!title || !contents) {
+    return res.status(400).json({
+      mesage: "Post needs a title or content",
     });
+  } else {
+    posts
+      .update(req.params.id, req.body)
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: "Error updating post" });
+      });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  if (req.params.id) {
+    posts
+      .remove(req.params.id)
+      .then(() => {
+        res.status(201).json({ message: "Post deleted" });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: "Error removing post" });
+      });
+  } else {
+  }
 });
 
 module.exports = router;
