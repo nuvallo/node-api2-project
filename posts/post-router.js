5dc35e6831a07dc3104a99d0b86f7f6c85c15424
nuvallo <nuvallo.inc@gmail.com>
2020-04-12 17:47:17 -0400

    GET posts complete

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const Posts = require("../data/db.js");
+const posts = require("../data/db.js");
 const router = express.Router();
 
 //GET request to /api/posts
 router.get("/", (req, res) => {
-  res.send("<h1>This worked</h1>");
+  posts.find().then((posts) => {
+    res
+      .status(200)
+      .json(posts)
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: "Error retrieving the posts" });
+      });
+  });
 });
 
 module.exports = router;
